068f9f64325264411954a06227c026ba39138614
adamgifford-behavr <84583381+adamgifford-behavr@users.noreply.github.com>
2022-08-03 15:18:03 -0400

    commented out pre-commit hooks that aren't working. will try to fix at later point

--- check-environment.sh
+++ check-environment.sh
@@ -1,39 +1,37 @@
 #!/usr/bin/env bash
 
 echo "checking environment.yaml file..."
-NEW_ENVIRONMENT=$(conda env export --no-builds | Out-File environment.yaml -Encoding utf8)
-
-if [ -f environment.yaml ]; then
-    echo "environment.yaml exists!"
-else
-    echo "FAILURE: environment.yaml does not exist!"
-    conda env export --no-builds | Out-File environment.yaml -Encoding utf8
-    exit 1
-fi
 
 ENVIRONMENT=$(cat environment.yaml)
+
+CONDA=`/mnt/c/Users/adamgifford_behavr.DESKTOP-PQU0D8M/anaconda3/Scripts/conda`
+
+NEW_ENVIRONMENT=$($CONDA env export > environment.yaml --no-builds)
+
+NEW_REQUIREMENTS=$(pip list --format=freeze)
+
+REQUIREMENTS=$(cat requirements.txt)
+
+echo "checking environment.yaml file..."
 
 if [ "$NEW_ENVIRONMENT" = "$ENVIRONMENT" ]; then
     echo "environment.yaml is up to date!"
     exit 0
 else
     echo "FAILURE: environment.yaml is not up to date!"
-    conda env export > environment.yaml --no-builds
+    $CONDA env export > environment.yaml --no-builds
+    exit 1
+fi
+
+if [ -f environment.yaml ]; then
+    echo "environment.yaml exists!"
+else
+    echo "FAILURE: environment.yaml does not exist!"
+    conda env export --no-builds
     exit 1
 fi
 
 echo "checking requirements.txt file..."
-NEW_REQUIREMENTS=$(pip list --format=freeze)
-
-if [ -f requirements.txt ]; then
-    echo "requirements.txt exists!"
-else
-    echo "FAILURE: requirements.txt does not exist!"
-    pip list --format=freeze > requirements.txt
-    exit 1
-fi
-
-REQUIREMENTS=$(cat requirements.txt)
 
 if [ "$NEW_REQUIREMENTS" = "$REQUIREMENTS" ]; then
     echo "requirements.txt is up to date!"
@@ -41,5 +39,14 @@ if [ "$NEW_REQUIREMENTS" = "$REQUIREMENTS" ]; then
 else
     echo "FAILURE: requirements.txt is not up to date!"
     pip list --format=freeze > requirements.txt
+    exit 1
+fi
+
+if [ -f requirements.txt ]; then
+    echo "requirements.txt exists!"
+else
+    echo "FAILURE: requirements.txt does not exist!"
+    pip list --format=freeze > requirements.txt
+    echo "-e .\n" >> requirements.txt
     exit 1
 fi
