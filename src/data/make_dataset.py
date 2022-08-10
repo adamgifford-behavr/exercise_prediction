88905652d5e39db843157d1da42ebd2d6a76373e
adamgifford-behavr <84583381+adamgifford-behavr@users.noreply.github.com>
2022-08-10 09:40:46 -0400

    continued notebook 1.2, created train-val-test split conditions

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -10,8 +10,9 @@ Args:
 """
 import json
 import logging
+from collections import defaultdict
 from pathlib import Path
-from typing import Optional, Union
+from typing import DefaultDict, List, Optional, Union
 
 import click
 import numpy as np
@@ -20,6 +21,8 @@ from dotenv import find_dotenv, load_dotenv
 from scipy import io as sio
 
 ACTIVITY_GROUPS_FILE = Path(__file__).parent / "activity_groupings.json"
+TRAIN_VAL_FILE = Path(__file__).parent / "train_val.json"
+TRAIN_TEST_FILE = Path(__file__).parent / "train-val_test.json"
 
 
 def _load_matfile(file: str) -> dict:
@@ -148,6 +151,134 @@ def _write_activity_groupings_json(useful_activity_groupings: np.ndarray) -> Non
         json.dump(activity_groupings, outfile)
 
 
+def _make_train_dataset(interim_path: str) -> None:
+    """foobar"""
+    logger = logging.getLogger(__name__)
+    logger.info("making training dataset")
+    print(interim_path)
+
+
+def _make_validation_dataset(interim_path: str) -> None:
+    """foobar"""
+    logger = logging.getLogger(__name__)
+    logger.info("making validation dataset")
+    print(interim_path)
+
+
+def _make_files_dict(all_files: list) -> DefaultDict[str, list]:
+    """
+    It takes a list of file paths and returns a dictionary of subject names and the list of
+    data id numbers associated with each subject
+
+    Args:
+      all_files (list): list of all files in the directory
+
+    Returns:
+      A dictionary with the subject as the key and the list of data ids as the value.
+    """
+    files_dict = defaultdict(list)
+    for file in all_files:
+        _, subj, _ = file.parts[-1].split("_")
+        files_dict[subj].append(str(file))
+
+    return files_dict
+
+
+def _get_first_subjs_match_crit(
+    files_dict: DefaultDict[str, list], n_sing_file: int, n_double_file: int
+) -> list:
+    """
+    It takes a dictionary of subject IDs and a list of files associated with each subject
+    ID, and returns a list of the first "n" subject IDs that match the criteria of having
+    either one (`n_sing_file`) or two (`n_double_file`) data files associated with them
+
+    Args:
+      files_dict (DefaultDict[str, list]): a dictionary of subject IDs and the list of
+      file ids they have.
+      n_sing_file (int): number of subjects with only one file to include in the test
+      dataset
+      n_double_file (int): number of subjects with two files to include in the test
+      dataset
+
+    Returns:
+      A list of the first subject IDs that match the criteria.
+    """
+    test_subj_ids = []
+    ones_left = n_sing_file
+    twos_left = n_double_file
+    for key, val in files_dict.items():
+        if not (ones_left or twos_left):
+            break
+
+        if (len(val) == 1) & (ones_left > 0):
+            test_subj_ids.append(key)
+            ones_left -= 1
+        elif (len(val) == 2) & (twos_left > 0):
+            test_subj_ids.append(key)
+            twos_left -= 1
+
+    return test_subj_ids
+
+
+def _make_train_test_dict(
+    all_files: List[Path], test_subj_ids: list
+) -> DefaultDict[str, list]:
+    """
+    It takes a dictionary of file paths and splits them into two lists, one for training
+    and one for testing.
+
+    Args:
+      all_files (DefaultDict[str, list]): a list of all the files in the directory
+      test_subj_ids (list): list of subject IDs to use for testing
+
+    Returns:
+      A dictionary with two keys: "test" and "train_val". The values are lists of strings
+      specifying the file paths of the train and test data.
+    """
+    train_test_files = defaultdict(list)
+    for file in all_files:
+        _, subj, _ = file.parts[-1].split("_")
+        if any(subj == test_subj for test_subj in test_subj_ids):
+            train_test_files["test"].append(str(file))
+        else:
+            train_test_files["train_val"].append(str(file))
+
+    return train_test_files
+
+
+def _make_train_test_split_json(
+    interim_path: Path, n_sing_file: int, n_double_file: int
+) -> None:
+    """
+    It takes a directory of files, and splits them into train and test sets, based on the
+    number of subjects in the test set that should have one (`n_file_single) or two
+    (`n_file_double`) data recording files. This function simply finds the first subjects
+    to meet these criteria by cycling through the files in `interim_path` in alphanumeric
+    order.
+
+    Args:
+      interim_path (Path): the path to the interim data folder
+      n_sing_file (int): number of single-data-file subjects to include in the test set
+      n_double_file (int): number of double-data-file subjects to include in the test set
+    """
+    all_files = list(x for x in interim_path.iterdir() if x.is_file())
+    files_dict = _make_files_dict(all_files)
+
+    test_subj_ids = _get_first_subjs_match_crit(files_dict, n_sing_file, n_double_file)
+
+    train_test_files = _make_train_test_dict(all_files, test_subj_ids)
+
+    with open("train-val_test.json", "w", encoding="utf-8") as outfile:
+        json.dump(train_test_files, outfile)
+
+
+def _make_test_dataset(interim_path: str):
+    """foobar"""
+    logger = logging.getLogger(__name__)
+    logger.info("making test dataset")
+    print(interim_path)
+
+
 def write_single_parquet_file_wrapper(
     interim_path: Union[str, Path],
     subj_data: np.ndarray,
@@ -213,6 +344,41 @@ def multi_convert_mat_to_parquet(
             write_single_parquet_file_wrapper(interim_path, subj_data, 0, overwrite)
 
 
+def _make_train_val_split_json(
+    interim_path: str, val_n_single: int, val_n_double: int
+) -> None:
+    """foobar"""
+    print([interim_path, val_n_single, val_n_double])
+
+
+def make_train_val_dataset(
+    interim_path: str,
+    val_n_single: int,
+    val_n_double: int,
+    test_n_single: int,
+    test_n_double: int,
+):
+    """foobar"""
+    logger = logging.getLogger(__name__)
+    if not TRAIN_TEST_FILE.exists():
+        logger.info(
+            "train/val vs. test split file does not exist. writing file first..."
+        )
+        _make_train_test_split_json(Path(interim_path), test_n_single, test_n_double)
+        logger.info("writing train/val-test file complete")
+
+    if not TRAIN_VAL_FILE.exists():
+        logger.info("train vs. val split file does not exist. writing file first...")
+        _make_train_val_split_json(interim_path, val_n_single, val_n_double)
+        logger.info("writing train-val file complete")
+
+    _make_train_dataset(interim_path)
+
+    _make_validation_dataset(interim_path)
+
+    _make_test_dataset(interim_path)
+
+
 @click.command()
 @click.argument(
     "input_filepath",
@@ -233,18 +399,62 @@ def multi_convert_mat_to_parquet(
     default=None,
 )
 @click.option(
-    "--overwrite_interim/--keep_interim",
+    "--overwrite-interim/--keep-interim",
     type=bool,
     default=False,
     show_default=True,
     help="Whether to overwrite files of matching interim data by filename if they already exist.",
 )
 @click.option(
-    "--overwrite_output/--keep_output",
+    "--overwrite-output/--keep-output",
     type=bool,
     default=False,
     show_default=True,
     help="Whether to overwrite files of matching output data by filename if they already exist.",
+)
+@click.option(
+    "-vns",
+    "--val-n-single",
+    type=int,
+    default=None,
+    show_default=True,
+    help=(
+        "Number of subjects in validation set that only have 1 data file recording. "
+        "Required only if 'train_val.json' doesn't exist."
+    ),
+)
+@click.option(
+    "-vnd",
+    "--val-n-double",
+    type=int,
+    default=None,
+    show_default=True,
+    help=(
+        "Number of subjects in validation set that have 2 data file recordings. "
+        "Required only if 'train_val.json' doesn't exist."
+    ),
+)
+@click.option(
+    "-tns",
+    "--test-n-single",
+    type=int,
+    default=13,
+    show_default=True,  # pylint: disable=too-many-arguments
+    help=(
+        "Number of subjects in test set that only have 1 data file recording. "
+        "Required only if 'train-val_test.json' doesn't exist."
+    ),
+)
+@click.option(
+    "-tnd",
+    "--test-n-double",
+    type=int,
+    default=6,
+    show_default=True,
+    help=(
+        "Number of subjects in test set that have 2 data file recordings. "
+        "Required only if 'train-val_test.json' doesn't exist."
+    ),
 )
 def main(
     input_filepath: Optional[str] = None,
@@ -252,12 +462,17 @@ def main(
     output_path: Optional[str] = None,
     overwrite_interim: Optional[bool] = False,
     overwrite_output: Optional[bool] = False,
+    val_n_single: Optional[int] = None,
+    val_n_double: Optional[int] = None,
+    test_n_single: Optional[int] = 13,
+    test_n_double: Optional[int] = 6,
 ) -> None:
     """
     Runs data processing scripts to turn raw data from (../raw) into
-    cleaned data ready to be analyzed (saved in ../interim/preprocessed). It requires an
-    intermediate step of converting data in a .mat file to a series of PARQUET files (one
-    file per subject and run through an exercise routine)
+    cleaned data ready to be analyzed (saved in ../interim/preprocessed). It requires
+    intermediate steps of converting data in a .mat file to a series of PARQUET files (one
+    file per subject and run through an exercise routine), separating data files into
+    train, validation, and test sets.
 
     Args:
       input_filepath (Optional[str]): location of the raw .mat file. Defaults to None,
@@ -296,6 +511,9 @@ def main(
     logger.info("creating preprocessed dataset from PARQUET files")
     if overwrite_output:
         logger.info("function will overwrite existing preprocessed data")
+        make_train_val_dataset(
+            interim_path, val_n_single, val_n_double, test_n_single, test_n_double
+        )
 
 
 if __name__ == "__main__":
