767b89a9f796afb25222ccf5d132e5fba1fe13fe
Adam Gifford <adamgifford@behavr.com>
2022-09-13 20:27:43 +0000

    updated prepare.py to save true reference data from training with predictions and only send feature data in simulation

--- src/monitor/prepare.py
+++ src/monitor/prepare.py
@@ -6,6 +6,7 @@ file to the evidently monitoring service directory.
 import os
 from pathlib import Path
 
+import mlflow
 from dotenv import find_dotenv, load_dotenv
 
 import src.models.score_batch as sb
@@ -18,11 +19,46 @@ sb.DATABASE_URI = DATABASE_URI
 sb.FEATURIZE_ID = os.getenv("FEATURIZE_ID")
 
 sim_df = sb.load_data("naive_frequency_features", "simulate")
-dst_paths = ("./", "./evidently_service/datasets/")
+train_df = sb.load_data("naive_frequency_features", "train", limit=sim_df.shape[0])
 
-file_path = Path(sim_df.loc[0, "file"])
-file_name = "preprocessed_" + file_path.parts[-1]
+stream_data_path = Path(sim_df.loc[0, "file"])
+stream_file_name = "preprocessed_" + stream_data_path.parts[-1]
+REF_FILE_NAME = "preprocessed_reference_data.parquet"
 
-for dst in dst_paths:
-    # ran into issues with fastparquet, but pyarrow seems to work...
-    sim_df.to_parquet(dst + file_name, engine="pyarrow")
+logged_model = os.getenv("MODEL_LOCATION", "prediction_service/models/")
+model = mlflow.pyfunc.load_model(logged_model)
+
+train_df = train_df.drop(
+    columns=[
+        "naive_frequency_features_id",
+        "featurize_id",
+        "file",
+        "dataset_group",
+        "added_datetime",
+        "window_size",
+        "t_index",
+        "label",
+    ]
+)
+
+sim_df = sim_df.drop(
+    columns=[
+        "naive_frequency_features_id",
+        "featurize_id",
+        "file",
+        "dataset_group",
+        "added_datetime",
+        "window_size",
+        "t_index",
+        "label",
+    ]
+)
+
+train_df["prediction"] = model.predict(
+    train_df.drop(columns=["label_group"]).to_dict("records")
+)
+REF_PATH = "./evidently_service/datasets/"
+STREAM_PATH = "./"
+
+train_df.to_parquet(REF_PATH + REF_FILE_NAME, engine="pyarrow")
+sim_df.to_parquet(STREAM_PATH + stream_file_name, engine="pyarrow")
