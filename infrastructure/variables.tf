4e99288ebe7ab376db803d84cc961bbf18b7ddfb
Adam Gifford <adamgifford@behavr.com>
2022-09-22 14:09:11 +0000

    updated main scripts and added base module script files

--- infrastructure/variables.tf
+++ infrastructure/variables.tf
@@ -5,23 +5,27 @@ variable "aws_region" {
 
 variable "project_id" {
   description = "project_id"
-  default = "mlops-zoomcamp"
+  default     = "exercise-prediction"
 }
 
 variable "source_stream_name" {
   description = ""
+  default     = "signals_stream"
 }
 
 variable "output_stream_name" {
   description = ""
+  default     = "predictions_stream"
 }
 
 variable "model_bucket" {
   description = "s3_bucket"
+  default     = "agifford-mlflow-artifacts-remote"
 }
 
 variable "lambda_function_local_path" {
   description = ""
+  default     = "src/deployment/streaming/"
 }
 
 variable "docker_image_local_path" {
@@ -30,8 +34,10 @@ variable "docker_image_local_path" {
 
 variable "ecr_repo_name" {
   description = ""
+  default     = "exercise_prediction_naive_feats_orch_cloud"
 }
 
 variable "lambda_function_name" {
   description = ""
+  default     = "lambda_function.py"
 }
