d0ce637fb8271dfb56eeb205f1213897b7ec88c2
Adam Gifford <adamgifford@behavr.com>
2022-09-27 19:19:42 +0000

    updated to attempt to fix failures in github ci actions

--- integration_tests/run.sh
+++ integration_tests/run.sh
@@ -8,6 +8,7 @@ if [ "${LOCAL_IMAGE_NAME}" == "" ]; then
     LOCAL_TAG=`date +"%Y-%m-%d-%H-%M"`
     export LOCAL_IMAGE_NAME="stream-exercise-prediction:${LOCAL_TAG}"
     echo "LOCAL_IMAGE_NAME is not set, building a new image with tag ${LOCAL_IMAGE_NAME}"
+    cp -r ../models ../src/deployment/streaming
     docker build -t ${LOCAL_IMAGE_NAME} ../src/deployment/streaming/
 else
     echo "no need to build image ${LOCAL_IMAGE_NAME}"
